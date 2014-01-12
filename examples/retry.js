2401e51c108343c8d59c0c169bf67d805348672b
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2014-01-11 21:26:19 -0500

    adding ability to set retry delay

--- examples/retry.js
+++ examples/retry.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var AWS = require('aws-sdk'),
-    cowork = require('../lib/cowork');
+cowork = require('../lib/cowork');
 
 AWS.config.loadFromPath(process.env.HOME + '/.ec2/credentials.json');
 
 var settings = {
   queueURL : 'https://sqs.us-east-1.amazonaws.com/967397977267/photo-service-dev-PhotoImportQueue-1QWK4DAIXQUE7',
-  concurrency: 100, // optional
+  concurrency: 30, // optional
   retryInterval : 10,
   notifications : { // optional
     error : 'arn:aws:sns:us-east-1:967397977267:PhotoImportFailed'
@@ -19,18 +19,16 @@ var start = new Date();
 
 var worker = function (job, callback) {
   console.log('basic processing: ', job);
-  setTimeout(function () {
-    cnt++;
-    if(job.foo % 10 === 0) {
-      var err = new Error('Failed but retryable');
-      err.retryable = true;
-      //return callback(err);
-      return callback();
-    } else {
-      console.log('finished', cnt, 'time - ' + (new Date() - start) + 'ms');
-      return callback();
-    }
-  }, 2000);
+  cnt++;
+  if(job.foo % 10 === 0) {
+    var err = new Error('Failed but retryable');
+    err.retryable = true;
+    err.delay = job.foo % 9;
+    return callback(err);
+  } else {
+    console.log('finished', cnt, 'time - ' + (new Date() - start) + 'ms');
+    return callback();
+  }
 };
 
 var jobs = cowork.createQueue(worker, settings);
